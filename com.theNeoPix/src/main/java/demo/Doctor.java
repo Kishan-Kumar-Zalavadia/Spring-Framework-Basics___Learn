4be01a98bb0d760525892c21cec53a1de314d10f
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-01-10 15:31:47 +0530

    Added properties for a class in xml

--- com.theNeoPix/src/main/java/demo/Doctor.java
+++ com.theNeoPix/src/main/java/demo/Doctor.java
@@ -1,7 +1,18 @@
 package demo;
 
-public class Doctor {
+public class Doctor implements Staff{
+
+    private String qualification;
+
     public void assist(){
         System.out.println("Doctor is Assisting");
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
     }
 }
--- com.theNeoPix/src/main/java/demo/Main.java
+++ com.theNeoPix/src/main/java/demo/Main.java
@@ -3,14 +3,27 @@ package demo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.print.Doc;
+
 public class Main {
     public static void main(String[] args){
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        //------------------------------------------------------------------
         Doctor doctor = context.getBean(Doctor.class);
         Nurse nurse1 = context.getBean(Nurse.class);
         Nurse nurse2 =(Nurse) context.getBean("nurse");
         doctor.assist();
         nurse2.assist();
+
+        //------------------------------------------------------------------
+        Staff staff = context.getBean(Doctor.class);
+        staff.assist();
+
+        //---------------------------------------------
+        Doctor doctor2 = context.getBean(Doctor.class);
+        staff.assist();
+        System.out.println(doctor2.getQualification());
     }
 }
