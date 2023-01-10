2fdd3cd32b0bf79e74c1ca7b4a46c102b3d7694b
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-01-10 15:36:21 +0530

    Adding Class as ref in properties

--- com.theNeoPix/src/main/java/demo/Doctor.java
+++ com.theNeoPix/src/main/java/demo/Doctor.java
@@ -3,6 +3,7 @@ package demo;
 public class Doctor implements Staff{
 
     private String qualification;
+    private Nurse nurse;
 
     public void assist(){
         System.out.println("Doctor is Assisting");
@@ -14,5 +15,13 @@ public class Doctor implements Staff{
 
     public void setQualification(String qualification) {
         this.qualification = qualification;
+    }
+
+    public Nurse getNurse() {
+        return nurse;
+    }
+
+    public void setNurse(Nurse nurse) {
+        this.nurse = nurse;
     }
 }
