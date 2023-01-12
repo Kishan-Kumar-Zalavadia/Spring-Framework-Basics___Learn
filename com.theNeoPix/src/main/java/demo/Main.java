694f5790e8b577328f7ca1afb8d53d5d35467c68
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-01-12 11:01:05 +0530

    Java Configuration

--- com.theNeoPix/src/main/java/demo/Main.java
+++ com.theNeoPix/src/main/java/demo/Main.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.print.Doc;
@@ -8,7 +9,9 @@ import javax.print.Doc;
 public class Main {
     public static void main(String[] args){
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 
         Staff staff1 = context.getBean(Doctor.class);
         staff1.assist();
